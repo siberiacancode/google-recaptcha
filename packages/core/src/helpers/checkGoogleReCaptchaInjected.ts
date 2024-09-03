cd0055e966c0567bf3fa13ada35cbea1ad64032a
debabin <dmitriybabinup@gmail.com>
2024-09-03 12:20:35 +0700

    release/1.2.2 🧊 optional id

--- packages/core/src/helpers/checkGoogleReCaptchaInjected.ts
+++ packages/core/src/helpers/checkGoogleReCaptchaInjected.ts
@@ -1,9 +1,9 @@
 /**
  * Checks if the Google reCAPTCHA script is injected in the document.
  *
- * @param {string} scriptId - The ID of the script element to check.
+ * @param {string} [scriptId] - The ID of the script element to check.
  * @return {boolean} Returns true if the Google reCAPTCHA script is injected, false otherwise.
  */
-export const checkGoogleReCaptchaInjected = (scriptId: string) =>
+export const checkGoogleReCaptchaInjected = (scriptId?: string) =>
   !!document.querySelector(`#${scriptId}`) ||
   !!document.querySelector('script[src*="/recaptcha/"]:not([src*="gstatic"])');
--- packages/core/src/helpers/removeGoogleReCaptchaScript.ts
+++ packages/core/src/helpers/removeGoogleReCaptchaScript.ts
@@ -3,20 +3,17 @@ export const GSTATIC_URL = 'https://www.gstatic.com/recaptcha/releases';
 /**
  * Removes the Google reCAPTCHA container element with the specified container ID.
  *
- * @param {ContainerId} containerId - The ID of the container element to be removed.
+ * @param {string} [scriptId] - The ID of the script element to check.
  * @return {void} This function does not return a value.
  */
-export const removeGoogleReCaptchaScript = (scriptId: string) => {
+export const removeGoogleReCaptchaScript = (scriptId?: string) => {
   (window as any).___grecaptcha_cfg = undefined;
 
-  const googleReCaptchaScript = document.querySelector(`#${scriptId}`);
-  if (googleReCaptchaScript) {
-    googleReCaptchaScript.remove();
-  }
+  const googleReCaptchaScript =
+    document.querySelector(`#${scriptId}`) ??
+    document.querySelector('script[src*="/recaptcha/"]:not([src*="gstatic"])');
+  if (googleReCaptchaScript) googleReCaptchaScript.remove();
 
   const gStaticScript = document.querySelector(`script[src^="${GSTATIC_URL}"]`);
-
-  if (gStaticScript) {
-    gStaticScript.remove();
-  }
+  if (gStaticScript) gStaticScript.remove();
 };
