559feeb17d44956b2a7a049155f9d4749af333b5
debabin <dmitriybabinup@gmail.com>
2025-03-29 18:51:36 +0700

    main 🔑 v2.1.0

--- packages/vue/src/components/index.ts
+++ packages/vue/src/components/index.ts
@@ -1,4 +1,3 @@
 export { default as GoogleReCaptchaCheckbox } from './GoogleReCaptchaCheckbox.vue';
 export { default as GoogleReCaptchaProvider } from './GoogleReCaptchaProvider.vue';
-export { default as GoogleReCaptchaV2Invisible } from './GoogleReCaptchaV2Invisible.vue';
 export { default as GoogleReCaptchaV3 } from './GoogleReCaptchaV3.vue';
--- packages/vue/vite.config.ts
+++ packages/vue/vite.config.ts
@@ -20,7 +20,10 @@ export default defineConfig({
       fileName: (format) => `index.${format}.js`
     },
     rollupOptions: {
-      external: [...Object.keys(pkg.peerDependencies || {})],
+      external: [
+        ...Object.keys(pkg.dependencies || {}),
+        ...Object.keys(pkg.peerDependencies || {})
+      ],
       output: [
         {
           format: 'es',
